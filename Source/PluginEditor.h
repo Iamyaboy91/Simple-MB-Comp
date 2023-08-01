b0af317040d672a240e3f048275db2cc8430eb33
Iamyaboy91 <yaboytaka.official@gmail.com>
2023-08-01 12:48:39 +0900

    added control to compressors Band controls

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -170,6 +170,12 @@ private:
                            releaseSliderAttachment,
                            thresholdSliderAttachment,
                            ratioSliderAttachment;
+    juce::ToggleButton bypassButton, soloButton, muteButton, lowBand, midBand, highBand;
+    
+    using BtnAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
+    unique_ptr<BtnAttachment> bypassButtonAttachment,
+                              soloButtonAttachment,
+                              muteButtonAttachment;
 };
 
 struct GlobalControls : juce::Component
